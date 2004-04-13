b886008774da656ab8418e46c861a0e19d284c57
Sashmit Bhaduri <smt@vfemail.net>
2004-04-13 06:46:33 +0000

    add some favicon infastructure (not used yet)
    svn path=/trunk/kdepim/akregator/; revision=303228

---
+++ src/feediconmanager.cpp
@@ -0,0 +1,94 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Sashmit Bhaduri                                 *
+ *   smt@vfemail.net                                                       *
+ *                                                                         *
+ *   Licensed under GPL.                                                   *
+ ***************************************************************************/
+
+#include "feediconmanager.h"
+
+#include <dcopclient.h>
+
+#include <kdebug.h>
+#include <kapplication.h>
+#include <kstandarddirs.h>
+#include <qpixmap.h>
+
+using namespace Akregator;
+
+FeedIconManager *FeedIconManager::m_instance = 0;
+
+FeedIconManager *FeedIconManager::self()
+{
+    if (!m_instance)
+        m_instance = new FeedIconManager();
+
+    return m_instance;
+}
+
+FeedIconManager::FeedIconManager(QObject * parent, const char *name)
+:  QObject(parent, name), DCOPObject("FeedIconManager")
+{
+    connectDCOPSignal("kded",
+                      "favicons", "iconChanged(bool, QString, QString)",
+                      "slotIconChanged(bool, QString, QString)", false);
+}
+
+FeedIconManager::~FeedIconManager()
+{
+    delete m_instance;
+}
+
+void FeedIconManager::loadIcon(const QString & url)
+{
+    if (url.isEmpty()) {
+        return;
+    }
+
+    KURL u(url);
+
+    QString iconFile = iconLocation(u);
+    if (iconFile.isNull()) {
+        QByteArray data;
+        QDataStream ds(data, IO_WriteOnly);
+        ds << u;
+        kapp->dcopClient()->send("kded", "favicons", "downloadHostIcon(KURL)",
+                                 data);
+    } else {
+        emit iconChanged(url,
+                         QPixmap(KGlobal::dirs()->
+                                 findResource("cache", iconFile)));
+    }
+}
+
+QString FeedIconManager::iconLocation(const KURL & url) const
+{
+    QByteArray data, reply;
+    QCString replyType;
+    QDataStream ds(data, IO_WriteOnly);
+
+    ds << url;
+
+    kapp->dcopClient()->call("kded", "favicons", "iconForURL(KURL)", data,
+                             replyType, reply);
+
+    if (replyType == "QString") {
+        QDataStream replyStream(reply, IO_ReadOnly);
+        QString result;
+        replyStream >> result;
+        return result;
+    }
+
+    return QString::null;
+}
+
+void FeedIconManager::slotIconChanged(bool isHost, QString hostOrURL,
+                                  QString iconName)
+{
+    emit iconChanged(hostOrURL,
+                     QPixmap(KGlobal::dirs()->
+                             findResource("cache", iconName)));
+}
+
+
+#include "feediconmanager.moc"
---
+++ src/feediconmanager.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Sashmit Bhaduri                                 *
+ *   smt@vfemail.net                                                       *
+ *                                                                         *
+ *   Licensed under GPL.                                                   *
+ ***************************************************************************/
+
+#ifndef FEEDICONMGR_H
+#define FEEDICONMGR_H
+
+#include <qobject.h>
+#include <qpixmap.h>
+
+#include <dcopobject.h>
+#include <kurl.h>
+
+class DCOPClient;
+
+
+namespace Akregator
+{
+    class FeedIconManager:public QObject, public DCOPObject
+    {
+        Q_OBJECT
+        K_DCOP
+        
+        public:
+            static FeedIconManager *self();
+            void loadIcon(const QString &);
+
+        k_dcop:
+            void slotIconChanged(bool, QString, QString);
+
+        signals:
+            void iconChanged(const QString &, const QPixmap &);
+
+        protected:
+            FeedIconManager(QObject * = 0L, const char * = 0L);
+            ~FeedIconManager();
+
+      private:
+            QString iconLocation(const KURL &) const;
+            static FeedIconManager *m_instance;
+    };
+}
+
+#endif
