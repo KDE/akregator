4f102a28c98944911af8fd9c5bf62273bcd5b98a
Sashmit Bhaduri <smt@vfemail.net>
2004-11-23 15:55:17 +0000

    change around for recent activity
    svn path=/trunk/kdepim/akregator/; revision=365521

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -18,11 +18,11 @@ AboutData::AboutData()
                  License_GPL, I18N_NOOP("(C) 2004 aKregator developers"), 0,
                      "http://akregator.sourceforge.net/")
 {
-    addAuthor( "Stanislav Karchebny", I18N_NOOP("Maintainer, Lead Developer"), "Stanislav.Karchebny@kdemail.net" );
-    addCredit( "Pierre Habouzit", I18N_NOOP("Release Manager"), "pierre.habouzit@m4x.org" );
-    addAuthor( "Sashmit Bhaduri", I18N_NOOP("Lead Developer"), "sashmit@vfemail.net" );
+    addAuthor( "Frank Osterfeld", I18N_NOOP("Maintainer"), "frank.osterfeld@kdemail.net" );
     addAuthor( "Teemu Rytilahti", I18N_NOOP("Developer"), "teemu.rytilahti@kde-fi.org" );
-    addAuthor( "Frank Osterfeld", I18N_NOOP("Developer"), "frank.osterfeld@kdemail.net" );
+    addAuthor( "Sashmit Bhaduri", I18N_NOOP("Developer"), "sashmit@vfemail.net" );
+    addAuthor( "Pierre Habouzit", I18N_NOOP("Developer"), "pierre.habouzit@m4x.org" );
+    addAuthor( "Stanislav Karchebny", I18N_NOOP("Developer"), "Stanislav.Karchebny@kdemail.net" );
     addAuthor( "Gary Cramblitt", I18N_NOOP("Contributor"), "garycramblitt@comcast.net");
     addAuthor( "Stephan Binner", I18N_NOOP("Contributor"), "binner@kde.org" );
     addAuthor( "Christof Musik", I18N_NOOP("Contributor"), "christof@freenet.de" );
