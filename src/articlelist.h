c46c0cea9568ff7f5be7472d8e9a83e36ec9989c
Frank Osterfeld <frank.osterfeld@kdemail.net>
2004-11-28 11:39:32 +0000

    added comments
    svn path=/trunk/kdepim/akregator/; revision=366848

--- src/articlelist.h
+++ src/articlelist.h
@@ -42,21 +42,44 @@ namespace Akregator
         public:
             ArticleList(QWidget *parent = 0, const char *name = 0);
             ~ArticleList();
-
+            
+            /**
+            Listen to notification signals of the viewed node. Disable this if you don't want this view updated when the node changes.
+            It's enabled by default.
+            
+            @param doReceive whether listen to notification signals or not
+            @param remember if @c true: if an update request occured while receiving updates was disabled, the view is updated on re-enabling. You have to set this when reenabling, not when disabling! Ignored when @c doReceive is set to false.(Anyone got this?) */
             void setReceiveUpdates(bool doReceive, bool remember=true);
             
         public slots:
+
+            /** show article list of tree node @c node (also connects to the notification signals of the node) */
             void slotShowNode(TreeNode* node);
+            
+            /** clears the list and disconnects from the observed node (if any) */
             void slotClear();
+            
+            /** Update view */
             void slotUpdate();
+            
+            /** sets text filter and status filter
+            @param textFilter filters text
+            @param statusFilter filters status (read, unread, new) */
             void slotSetFilter(const ArticleFilter& textFilter, const ArticleFilter& statusFilter);
+
+            /** selects previous article in list view, first article if no article was selected */
             void slotPreviousArticle();
+            /** selects next article in list view, first article if no article was selected */
             void slotNextArticle();
+            /** selects previous unread article in list view, first unread article if no article was selected */
             void slotPreviousUnreadArticle();
+            /** selects next unread article in list view, first unread article if no article was selected */
             void slotNextUnreadArticle();
 
         signals:
-             void signalArticleSelected(MyArticle article);
+             /** emitted when an article was selected
+                 @param article the article selected*/
+            void signalArticleSelected(MyArticle article);
              
         protected:
             /** reimplemented for kmail-like behaviour */            
@@ -67,7 +90,7 @@ namespace Akregator
             virtual void applyFilters();
 
         protected slots:
-            virtual void slotSelectionChanged(QListViewItem* item);    
+            virtual void slotSelectionChanged(QListViewItem* item);
             
         private:
             bool m_updated;
