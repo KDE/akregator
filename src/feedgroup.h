8b006bac8178cfaa0fe78fb60f0ca9deb5fb7e79
Frank Osterfeld <frank.osterfeld@kdemail.net>
2005-03-02 21:57:55 +0000

    change license headers
    svn path=/trunk/kdepim/akregator/; revision=394461

--- src/feedgroup.h
+++ src/feedgroup.h
@@ -1,9 +1,27 @@
-/***************************************************************************
- *   Copyright (C) 2004 by Stanislav Karchebny                             *
- *   Stanislav.Karchebny@kdemail.net                                       *
- *                                                                         *
- *   Licensed under GPL.                                                   *
- ***************************************************************************/
+/*
+    This file is part of Akregator.
+
+    Copyright (C) 2004 Stanislav Karchebny <Stanislav.Karchebny@kdemail.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
+    without including the source code for Qt in the source distribution.
+*/
+
 #ifndef AKREGATORFEEDGROUP_H
 #define AKREGATORFEEDGROUP_H
 
