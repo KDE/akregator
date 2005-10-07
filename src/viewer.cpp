886ebc1d0ca7c129138298c6df7f4fd5baf109b6
Frank Osterfeld <frank.osterfeld@kdemail.net>
2005-10-07 08:09:41 +0000

    Add myself to copyright headers of files where I did significant changes SVN_SILENT
    svn path=/branches/KDE/3.5/kdepim/; revision=468124

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -2,6 +2,7 @@
     This file is part of Akregator.
 
     Copyright (C) 2004 Teemu Rytilahti <tpr@d5k.net>
+                  2005 Frank Osterfeld <frank.osterfeld at kdemail.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
