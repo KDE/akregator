9310e69dd27ed5c59c979108846519d182032dc0
Frank Osterfeld <frank.osterfeld@kdemail.net>
2005-12-14 16:45:30 +0000

    more complete apidox
    svn path=/trunk/KDE/kdepim/akregator/; revision=488470

--- src/libsyndication/src/rss2/item.h
+++ src/libsyndication/src/rss2/item.h
@@ -39,7 +39,7 @@ class Enclosure;
 class Source;
 
 /**
- * An Item, representing the entries in an RSS feed.
+ * An Item, representing an entry in an RSS feed.
  *
  * @author Frank Osterfeld
  */
@@ -109,29 +109,53 @@ class Item
 
         /**
          * The title of the item.
+         * 
+         * @return The title in plain text. Note that e.g. characters like <,
+         * >, & are not escaped!
+         * (TODO: this might change, check what makes more sense)
          */
         QString title() const;
 
         /**
-         * The URL of the item.
+         * The URL of the item. This usually links to the web representation
+         * of the item, e.g. the full news article.
+         * 
+         * @return an URL, or QString::null if not set
          */
         QString link() const;
 
         /**
-         * The item synopsis.
+         * The item synopsis. This might contain a short summary of the
+         * item, but also the full content. If content() is set, that usually
+         * contains the full content instead.
+         * 
+         * @return a string in HTML format (whitespace is irrelevant, <br/> is
+         * used for newlines, "&", "<", ">" are escaped) summarizing the item.
+         * QString::null if no description was specified.
          */
         QString description() const;
     
-        /** returns data stored in content:encoded, xhtml:body, xhtml:div */
+        /**
+         * Returns the actual content of the item. In RSS2, this can be stored
+         * in multiple structs, e.g. in content:encoded, xhtml:body or
+         * xhtml:div. If this is not set, description() might also contain the
+         * content of the item.
+         * 
+         * @return the content in HTML format (whitespace is irrelevant, <br/> is used for newlines, "&", "<", ">" are escaped) If no content is specified, QString::null is returned.
+         */
         QString content() const;
 
         /**
          * Set of categories this item is included in.
+         * 
+         * @return a list of categories, possibly empty.
          */
         QList<Category> categories() const;
 
         /**
          * URL of a page for comments relating to the item.
+         * 
+         * @return an URL to the comments, or QString::null if not set
          */
         QString comments() const;
 
@@ -141,12 +165,18 @@ class Item
          * the article that this item describes. For collaborative weblogs, the
          * author of the item might be different from the managing editor or
          * webmaster.
+         * 
+         * @return an email address of the author, or QString::null if not
+         * specified
          */
         QString author() const;
 
         /**
          * Describes a media object that is attached to the item.
          * See Enclosure for more details.
+         * 
+         * @return an enclosure object if set, or a null object (see
+         * Enclosure.isNull()) otherwise.
          */
         Enclosure enclosure() const;
 
@@ -157,6 +187,9 @@ class Item
          * There are no rules for the syntax of a guid. Aggregators must view
          * them as a string. It's up to the source of the feed to establish the
          * uniqueness of the string."
+         * 
+         * @return a guid string, or QString::null if none specified in the
+         * feed
          */
         QString guid() const;
 
@@ -166,24 +199,31 @@ class Item
          * points to the full item.
          *
          * @return @c true if the guid is a permalink and can be interpreted as
-         *                 URL
+         * URL
          */
          bool guidIsPermaLink() const;
 
         /**
          * Indicates when the item was published. If it's a date in the future,
-         * yous may choose to not display the item until that date. 
+         * yous may choose to not display the item until that date.
+         * 
+         * @return the publication date, or an invalid QDateTime object if
+         * either no date was specified or parsing failed.
          */
         QDateTime pubDate() const;
 
         /** 
-         * The RSS channel that the item came from.
+         * The RSS channel that the item came from. See Source class for more
+         * information.
+         * 
+         * @return a Source object, or a null object (see Source.isNull()) if
+         * not set.
          */
         Source source() const;
     
         /**
-         * Returns a description of the object and its
-         * children for debugging purposes.
+         * Returns a description of the object and its children for debugging
+         * purposes.
          *
          * @return debug string
          */
