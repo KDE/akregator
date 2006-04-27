8082ffd807a6793105b32813d2b4e5307fbb8f2f
Dirk Mueller <mueller@kde.org>
2006-04-27 08:31:59 +0000

    fix crash (CID 1502)
    svn path=/trunk/KDE/kdepim/akregator/; revision=534492

--- src/folderitem.cpp
+++ src/folderitem.cpp
@@ -55,7 +55,7 @@ FolderItem::FolderItem(K3ListView* parent, TreeNodeItem* after, Folder* node) : 
 
 void FolderItem::initialize(Folder* node)
 {
-    setOpen(node->isOpen());
+    setOpen(node && node->isOpen());
     setPixmap ( 0, KGlobal::iconLoader()->loadIcon("folder", K3Icon::Small) );
     if (node)
         setText(0, node->title());
