f1ca47182792c7e1e0c0be584d9de0da88e3f307
Urs Wolfer <uwolfer@kde.org>
2008-02-17 21:48:30 +0000

    Only apply old column widths if there are valid sizes
    svn path=/trunk/KDE/kdepim/akregator/; revision=776355

--- src/articlelistview.cpp
+++ src/articlelistview.cpp
@@ -237,12 +237,13 @@ void Akregator::ArticleListView::loadHeaderSettings()
     if ( !s.isNull() )
         header()->restoreState( s );*/
     QList<int> columnsSize = Settings::articlelistHeaderStates();
-    for (int i = 0; i != columnsSize.count(); i++)
-    {
-        kDebug() << i << " " << columnsSize.at( i );
-        setColumnWidth( i, columnsSize.at( i ) );
-    }//FIXME: HACK: Change back to saveState() when the Qt-bug is fixed
-
+    if ( !columnsSize.isEmpty() ) {
+        for (int i = 0; i != columnsSize.count(); i++)
+        {
+            kDebug() << i << " " << columnsSize.at( i );
+            setColumnWidth( i, columnsSize.at( i ) );
+        }//FIXME: HACK: Change back to saveState() when the Qt-bug is fixed
+    }
     header()->setSortIndicator( Settings::articlelistSortColumn(), ( Settings::articlelistSortOrder() == 1 ? Qt::AscendingOrder : Qt::DescendingOrder ) );
     sortByColumn( Settings::articlelistSortColumn(), ( Settings::articlelistSortOrder() == 1 ? Qt::AscendingOrder : Qt::DescendingOrder ) );
 }
