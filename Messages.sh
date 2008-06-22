53a8dc8a84637c7c4fccb9cd627ff5815effd557
Albert Astals Cid <tsdgeos@terra.es>
2008-06-22 14:41:52 +0000

    Move Messages.sh one level up so .ui files in configuration are extracted too
    svn path=/trunk/KDE/kdepim/akregator/; revision=823079

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
-$XGETTEXT *.cpp -o $podir/akregator.pot
+$XGETTEXT $(find -name "*.cpp" ) -o $podir/akregator.pot
