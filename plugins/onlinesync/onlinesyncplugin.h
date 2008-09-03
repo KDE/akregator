0947cdebd761fa8ed18514f8bcba3a1b9d55de25
Didier Hoarau <did.hoarau@gmail.com>
2008-09-03 20:31:55 +0000

    Copyright added
    svn path=/trunk/KDE/kdepim/akregator/; revision=856813

--- plugins/onlinesync/onlinesyncplugin.h
+++ plugins/onlinesync/onlinesyncplugin.h
@@ -1,7 +1,7 @@
 /*
     This file is part of Akregator.
 
-    Copyright (C) 2008 Frank Osterfeld <osterfeld@kde.org>
+    Copyright (C) 2008 Didier Hoarau <did.hoarau@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
