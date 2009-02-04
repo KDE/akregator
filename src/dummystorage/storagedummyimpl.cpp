366f981e5c8a98d189d1b35406d098f375355909
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2009-02-04 20:13:46 +0000

    Minor and harmless copyright changes.
    svn path=/trunk/KDE/kdepim/akregator/; revision=921333

--- src/dummystorage/storagedummyimpl.cpp
+++ src/dummystorage/storagedummyimpl.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of Akregator.
 
-    2005 Frank Osterfeld <osterfeld@kde.org>
+    Copyright 2005 Frank Osterfeld <osterfeld@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
