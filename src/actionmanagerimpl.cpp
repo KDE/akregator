dc47e657e52848b8d6f52f07486573ae70ba071e
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2010-06-10 12:29:59 +0000

    "Fix the icons and shortcuts with RTL. BUG: 205288 MERGE:4.4 FIXED-IN: 1.6.5
    svn path=/trunk/KDE/kdepim/akregator/; revision=1136672

--- src/actionmanagerimpl.cpp
+++ src/actionmanagerimpl.cpp
@@ -54,6 +54,7 @@
 #include <kxmlguifactory.h>
 #include <kicon.h>
 
+#include <QApplication>
 #include <QHash>
 #include <QWidget>
 
@@ -558,17 +559,19 @@ void ActionManagerImpl::initFrameManager(FrameManager* frameManager)
         return;
 
     d->frameManager = frameManager;
+    
+    bool isRTL = QApplication::isRightToLeft();
 
-    KToolBarPopupAction* forward = new KToolBarPopupAction(KIcon("go-next"), i18nc("Go forward in browser history", "Forward"), this);
+    KToolBarPopupAction* forward = new KToolBarPopupAction(KIcon(isRTL ? "go-previous" : "go-next"), i18nc("Go forward in browser history", "Forward"), this);
     d->actionCollection->addAction("browser_forward", forward);
-    forward->setShortcuts(KShortcut("Alt+Right"));
+    forward->setShortcuts(KShortcut(isRTL ? "Alt+Left" : "Alt+Right"));
     connect(forward, SIGNAL(triggered()), frameManager, SLOT(slotBrowserForward()));
 
     connect(forward->menu(), SIGNAL(aboutToShow()), frameManager, SLOT(slotBrowserForwardAboutToShow()));
 
-    KToolBarPopupAction* back = new KToolBarPopupAction(KIcon("go-previous"), i18nc("Go back in browser history", "Back"), this);
+    KToolBarPopupAction* back = new KToolBarPopupAction(KIcon(isRTL ? "go-next" : "go-previous"), i18nc("Go back in browser history", "Back"), this);
     d->actionCollection->addAction("browser_back", back);
-    back->setShortcuts(KShortcut("Alt+Left"));
+    back->setShortcuts(KShortcut(isRTL ?  "Alt+Right" : "Alt+Left"));
     connect(back, SIGNAL(triggered()), frameManager, SLOT(slotBrowserBack()));
 
     connect(back->menu(), SIGNAL(aboutToShow()), frameManager, SLOT(slotBrowserBackAboutToShow()));
