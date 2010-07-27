59159a482e6cdd1002ecf987b7d89fe04d9f8c1a
Romain Pokrzywka <romain@kdab.net>
2010-07-27 12:50:51 +0000

    added static library support for kdepim, and enable it for WinCE
    svn path=/trunk/KDE/kdepim/kmail/; revision=1155485

--- interfaces/akregator_export.h
+++ interfaces/akregator_export.h
@@ -24,7 +24,10 @@
 #include <kdemacros.h>
 
 #ifndef AKREGATOR_EXPORT
-# if defined(MAKE_AKREGATORINTERFACES_LIB) || defined(MAKE_AKREGATORPRIVATE_LIB)
+# if defined(KDEPIM_STATIC_LIBS)
+   /* No export/import for static libraries */
+#  define AKREGATOR_EXPORT
+# elif defined(MAKE_AKREGATORINTERFACES_LIB) || defined(MAKE_AKREGATORPRIVATE_LIB)
    /* We are building this library */ 
 #  define AKREGATOR_EXPORT KDE_EXPORT
 # else
@@ -34,7 +37,10 @@
 #endif
 
 #ifndef AKREGATORPART_EXPORT
-# if defined(MAKE_AKREGATORPART_LIB)
+# if defined(KDEPIM_STATIC_LIBS)
+   /* No export/import for static libraries */
+#  define AKREGATORPART_EXPORT
+# elif defined(MAKE_AKREGATORPART_LIB)
    /* We are building this library */ 
 #  define AKREGATORPART_EXPORT KDE_EXPORT
 # else
@@ -44,7 +50,10 @@
 #endif
 
 #ifndef AKREGATORINTERFACES_EXPORT
-# if defined(MAKE_AKREGATORINTERFACES_LIB)
+# if defined(KDEPIM_STATIC_LIBS)
+   /* No export/import for static libraries */
+#  define AKREGATORINTERFACES_EXPORT
+# elif defined(MAKE_AKREGATORINTERFACES_LIB)
    /* We are building this library */
 #  define AKREGATORINTERFACES_EXPORT KDE_EXPORT
 # else
