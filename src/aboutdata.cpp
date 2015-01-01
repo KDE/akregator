01671efc4d44f54a42a1b7093d45d139f1292676
Montel Laurent <montel@kde.org>
2015-01-01 12:34:00 +0100

    Update copyright

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -33,7 +33,7 @@ namespace Akregator
 
 AboutData::AboutData()
     : K4AboutData("akregator", 0, ki18n("Akregator"), KDEPIM_VERSION, ki18n("A KDE Feed Reader"),
-                  License_GPL, ki18n("Copyright © 2004–2010 Akregator authors"), KLocalizedString(),
+                  License_GPL, ki18n("Copyright © 2004–2015 Akregator authors"), KLocalizedString(),
                   "http://akregator.kde.org/")
 {
     setOrganizationDomain("kde.org"); // needed to get org.kde.akregator used for DBUS
