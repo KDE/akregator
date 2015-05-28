af95aad09549b3392d0eafcc50ac288c2643d0ec
Montel Laurent <montel@kde.org>
2015-05-28 23:42:44 +0200

    Not necessary to link with IconThem

---
+++ src/akregratormigrateapplication.h
@@ -0,0 +1,16 @@
+#ifndef AKREGRATORMIGRATEAPPLICATION_H
+#define AKREGRATORMIGRATEAPPLICATION_H
+
+
+class AkregratorMigrateApplication
+{
+    Q_OBJECT
+public:
+    AkregratorMigrateApplication();
+
+signals:
+
+public slots:
+};
+
+#endif // AKREGRATORMIGRATEAPPLICATION_H
