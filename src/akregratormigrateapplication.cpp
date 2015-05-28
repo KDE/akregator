a0ee43934c840549b7c47411ebc9e35357769807
Montel Laurent <montel@kde.org>
2015-05-28 23:44:06 +0200

    Improve migrate data

--- src/akregratormigrateapplication.cpp
+++ src/akregratormigrateapplication.cpp
@@ -1,7 +1,48 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "akregratormigrateapplication.h"
+
+#include <kdelibs4configmigrator.h>
 
 AkregratorMigrateApplication::AkregratorMigrateApplication()
 {
-
+    initializeMigrator();
 }
 
+void AkregratorMigrateApplication::migrate()
+{
+    // Migrate to xdg.
+    Kdelibs4ConfigMigrator migrate(QStringLiteral("akregator"));
+    migrate.setConfigFiles(QStringList() << QStringLiteral("akregatorrc") << QStringLiteral("akregator.notifyrc"));
+    migrate.setUiFiles(QStringList() << QStringLiteral("akregator_shell.rc") << QStringLiteral("akregator_part.rc") << QStringLiteral("pageviewer.rc")
+                       << QStringLiteral("articleviewer.rc") << QStringLiteral("akregator_sharemicroblog_plugin.rc") << QStringLiteral("akregator_onlinesync_plugin.rc"));
+    migrate.migrate();
+
+    // Migrate folders and files.
+    if (mMigrator.checkIfNecessary()) {
+        mMigrator.start();
+    }
+}
+
+void AkregratorMigrateApplication::initializeMigrator()
+{
+    mMigrator.setApplicationName(QStringLiteral("akreagator"));
+    mMigrator.setConfigFileName(QStringLiteral("akregatorrc"));
+    mMigrator.setCurrentConfigVersion(1);
+    //TODO
+}
--- src/akregratormigrateapplication.h
+++ src/akregratormigrateapplication.h
@@ -1,16 +1,33 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef AKREGRATORMIGRATEAPPLICATION_H
 #define AKREGRATORMIGRATEAPPLICATION_H
 
+#include "pimcommon/migration/migrateapplicationfiles.h"
 
 class AkregratorMigrateApplication
 {
-    Q_OBJECT
 public:
     AkregratorMigrateApplication();
-
-signals:
-
-public slots:
+    void migrate();
+private:
+    void initializeMigrator();
+    PimCommon::MigrateApplicationFiles mMigrator;
 };
 
 #endif // AKREGRATORMIGRATEAPPLICATION_H
