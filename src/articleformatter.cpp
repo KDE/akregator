c367ba324c0eb37ab38d6e0fb83b32c90c851b9f
Montel Laurent <montel@kde.org>
2015-08-03 13:40:50 +0200

    Astyle kdelibs

--- src/articleformatter.cpp
+++ src/articleformatter.cpp
@@ -295,9 +295,9 @@ QString DefaultNormalViewFormatter::getCss() const
                 pal.color(QPalette::Background).name(),
                 pal.color(QPalette::Text).name());
     css += QString::fromLatin1(".headertitle a:link { color: %1 ! important;\n text-decoration: none ! important;\n }\n"
-                   ".headertitle a:visited { color: %1 ! important;\n text-decoration: none ! important;\n }\n"
-                   ".headertitle a:hover{ color: %1 ! important;\n text-decoration: none ! important;\n }\n"
-                   ".headertitle a:active { color: %1 ! important;\n  text-decoration: none ! important;\n }\n")
+                               ".headertitle a:visited { color: %1 ! important;\n text-decoration: none ! important;\n }\n"
+                               ".headertitle a:hover{ color: %1 ! important;\n text-decoration: none ! important;\n }\n"
+                               ".headertitle a:active { color: %1 ! important;\n  text-decoration: none ! important;\n }\n")
            .arg(pal.color(QPalette::HighlightedText).name());
     css += QString(
                ".headertitle {\n"
@@ -484,9 +484,9 @@ QString DefaultCombinedViewFormatter::getCss() const
                 pal.color(QPalette::Text).name());
 
     css += QString::fromLatin1(".headertitle a:link { color: %1  ! important; text-decoration: none ! important;\n }\n"
-                   ".headertitle a:visited { color: %1 ! important; text-decoration: none ! important;\n }\n"
-                   ".headertitle a:hover{ color: %1 ! important; text-decoration: none ! important;\n }\n"
-                   ".headertitle a:active { color: %1 ! important; text-decoration: none ! important;\n }\n")
+                               ".headertitle a:visited { color: %1 ! important; text-decoration: none ! important;\n }\n"
+                               ".headertitle a:hover{ color: %1 ! important; text-decoration: none ! important;\n }\n"
+                               ".headertitle a:active { color: %1 ! important; text-decoration: none ! important;\n }\n")
            .arg(pal.color(QPalette::HighlightedText).name());
     css += QString(
                ".headertitle {\n"
