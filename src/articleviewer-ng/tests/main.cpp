b0df632509bbcad9264ff09c9c1cdad2108e2900
Montel Laurent <montel@kde.org>
2016-01-04 13:36:16 +0100

    improve application

--- src/articleviewer-ng/tests/main.cpp
+++ src/articleviewer-ng/tests/main.cpp
@@ -15,6 +15,8 @@
   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include "articleviewerngtest.h"
+
 #include <QStandardPaths>
 #include <QApplication>
 #include <QCommandLineParser>
@@ -36,6 +38,8 @@ int main(int argc, char **argv)
     parser.process(app);
     aboutData.processCommandLine(&parser);
 
+    ArticleViewerNgTest *article = new ArticleViewerNgTest;
+    article->show();
     app.exec();
     return 0;
 }
