15fbae9c0316f792884dc94bae1c5ff2c8d9f7f2
Montel Laurent <montel@kde.org>
2016-01-05 07:34:05 +0100

    Add code to generate html

---
+++ src/articleviewer-ng/articlehtmlwriter.h
@@ -0,0 +1,41 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef ARTICLEHTMLWRITER_H
+#define ARTICLEHTMLWRITER_H
+
+#include <QObject>
+class QWebView;
+namespace Akregator
+{
+class ArticleHtmlWriter : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ArticleHtmlWriter(QWebView *view, QObject *parent = Q_NULLPTR);
+    ~ArticleHtmlWriter();
+
+    void begin();
+    void end();
+
+private:
+    QString mHtml;
+    QWebView *mWebView;
+};
+}
+
+#endif // ARTICLEHTMLWRITER_H
