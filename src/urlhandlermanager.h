7c2185f27570400c88017a772f1f94d5d8b931ab
Montel Laurent <montel@kde.org>
2016-01-07 21:20:15 +0100

    Start to implement url handler

---
+++ src/urlhandlermanager.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef URLHANDLERMANAGER_H
+#define URLHANDLERMANAGER_H
+
+#include <QObject>
+
+namespace Akregator
+{
+class URLHandlerManager : public QObject
+{
+    Q_OBJECT
+public:
+    ~URLHandlerManager();
+
+    URLHandlerManager *instance();
+
+private:
+    explicit URLHandlerManager(QObject *parent = Q_NULLPTR);
+    static URLHandlerManager *self;
+};
+}
+
+#endif // URLHANDLERMANAGER_H
