1d42e1a1e2c7a39c93049b19fb3770e20a18ac8e
Montel Laurent <montel@kde.org>
2016-02-10 07:37:34 +0100

    Start to look at webengine support

---
+++ src/frame/webengine/webengineframe.cpp
@@ -0,0 +1,238 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "openurlrequest.h"
+#include "webengineframe.h"
+#include "webviewer.h"
+#include <QVBoxLayout>
+#include <QAction>
+#include <KIO/FavIconRequestJob>
+using namespace Akregator;
+
+WebEngineFrame::WebEngineFrame(KActionCollection *ac, QWidget *parent)
+    : Frame(parent)
+{
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->setMargin(0);
+    setRemovable(true);
+    Akregator::WebViewer *viewer = new Akregator::WebViewer(ac, this);
+    mArticleViewerWidgetNg = new Akregator::ArticleViewerWidgetNg(viewer, ac, this);
+
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::titleChanged, this, &WebEngineFrame::slotTitleChanged);
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::loadProgress, this, &WebEngineFrame::slotProgressChanged);
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::signalOpenUrlRequest, this, &WebEngineFrame::signalOpenUrlRequest);
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::loadStarted, this, &WebEngineFrame::slotLoadStarted);
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::loadFinished, this, &WebEngineFrame::slotLoadFinished);
+    connect(mArticleViewerWidgetNg->articleViewerNg(), &ArticleViewerNg::showStatusBarMessage, this, &WebEngineFrame::showStatusBarMessage);
+    layout->addWidget(mArticleViewerWidgetNg);
+}
+
+WebEngineFrame::~WebEngineFrame()
+{
+}
+
+void WebEngineFrame::slotLoadFinished()
+{
+    Q_EMIT signalCompleted(this);
+}
+
+void WebEngineFrame::slotLoadStarted()
+{
+    Q_EMIT signalStarted(this);
+}
+
+void WebEngineFrame::slotProgressChanged(int progress)
+{
+    Q_EMIT signalLoadingProgress(this, progress);
+}
+
+void WebEngineFrame::slotTitleChanged(const QString &title)
+{
+    Q_EMIT signalTitleChanged(this, title);
+}
+
+KParts::ReadOnlyPart *WebEngineFrame::part() const
+{
+    return Q_NULLPTR;
+}
+
+QUrl WebEngineFrame::url() const
+{
+    return mArticleViewerWidgetNg->articleViewerNg()->url();
+}
+
+bool WebEngineFrame::openUrl(const OpenUrlRequest &request)
+{
+    const QUrl url = request.url();
+    KIO::FavIconRequestJob *job = new KIO::FavIconRequestJob(url);
+    connect(job, &KIO::FavIconRequestJob::result, this, [job, this](KJob *){
+        if (!job->error()) {
+            Q_EMIT signalIconChanged(this, QIcon(job->iconFile()));
+        }
+    });
+
+
+
+    mArticleViewerWidgetNg->articleViewerNg()->load(url);
+    return true;
+}
+
+void WebEngineFrame::loadConfig(const KConfigGroup &config, const QString &prefix)
+{
+    const QString url = config.readEntry(QStringLiteral("url").prepend(prefix), QString());
+    const qreal zf = config.readEntry(QStringLiteral("zoom").prepend(prefix), 1.0);
+    const bool onlyZoomFont = config.readEntry(QStringLiteral("onlyZoomFont").prepend(prefix), false);
+    OpenUrlRequest req(url);
+    KParts::OpenUrlArguments args;
+    req.setArgs(args);
+    openUrl(req);
+    mArticleViewerWidgetNg->articleViewerNg()->setZoomFactor(zf);
+    mArticleViewerWidgetNg->articleViewerNg()->settings()->setAttribute(QWebSettings::ZoomTextOnly, onlyZoomFont);
+}
+
+void WebEngineFrame::saveConfig(KConfigGroup &config, const QString &prefix)
+{
+    config.writeEntry(QStringLiteral("url").prepend(prefix), url().url());
+    config.writeEntry(QStringLiteral("zoom").prepend(prefix), mArticleViewerWidgetNg->articleViewerNg()->zoomFactor());
+    config.writeEntry(QStringLiteral("onlyZoomFont").prepend(prefix), mArticleViewerWidgetNg->articleViewerNg()->settings()->testAttribute(QWebSettings::ZoomTextOnly));
+}
+
+void WebEngineFrame::slotCopyInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotCopy();
+}
+
+void WebEngineFrame::slotPrintInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotPrint();
+}
+
+void WebEngineFrame::slotPrintPreviewInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotPrintPreview();
+}
+
+void WebEngineFrame::slotFindTextInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->slotFind();
+}
+
+void WebEngineFrame::slotTextToSpeechInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->slotSpeakText();
+}
+
+void WebEngineFrame::slotZoomChangeInFrame(int frameId, qreal value)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->setZoomFactor(value);
+}
+
+void WebEngineFrame::slotZoomTextOnlyInFrame(int frameId, bool textOnlyInFrame)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotZoomTextOnlyInFrame(textOnlyInFrame);
+}
+
+void WebEngineFrame::slotHistoryForward()
+{
+    mArticleViewerWidgetNg->articleViewerNg()->pageAction(QWebPage::Forward)->trigger();
+}
+
+void WebEngineFrame::slotHistoryBack()
+{
+    mArticleViewerWidgetNg->articleViewerNg()->pageAction(QWebPage::Back)->trigger();
+}
+
+void WebEngineFrame::slotReload()
+{
+    mArticleViewerWidgetNg->articleViewerNg()->reload();
+}
+
+void WebEngineFrame::slotStop()
+{
+    mArticleViewerWidgetNg->articleViewerNg()->stop();
+}
+
+qreal WebEngineFrame::zoomFactor() const
+{
+    return mArticleViewerWidgetNg->articleViewerNg()->zoomFactor();
+}
+
+bool WebEngineFrame::zoomTextOnlyInFrame() const
+{
+    return mArticleViewerWidgetNg->articleViewerNg()->zoomTextOnlyInFrame();
+}
+
+void WebEngineFrame::slotSaveLinkAsInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotSaveLinkAs();
+}
+
+void WebEngineFrame::slotCopyLinkAsInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotCopyLinkAddress();
+}
+
+void WebEngineFrame::slotSaveImageOnDiskInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotSaveImageOnDiskInFrame();
+}
+
+void WebEngineFrame::slotCopyImageLocationInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotCopyImageLocationInFrame();
+}
+
+void WebEngineFrame::slotBlockImageInFrame(int frameId)
+{
+    if (frameId != id()) {
+        return;
+    }
+    mArticleViewerWidgetNg->articleViewerNg()->slotBlockImage();
+}
---
+++ src/frame/webengine/webengineframe.h
@@ -0,0 +1,77 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef WEBENGINEFRAME_H
+#define WEBENGINEFRAME_H
+
+#include "articleviewer-ng/articleviewerwidgetng.h"
+#include "frame.h"
+#include "akregatorpart_export.h"
+#include <QObject>
+
+namespace Akregator
+{
+class AKREGATORPART_EXPORT WebEngineFrame : public Frame
+{
+    Q_OBJECT
+public:
+    explicit WebEngineFrame(KActionCollection *ac, QWidget *parent = Q_NULLPTR);
+    ~WebEngineFrame();
+
+    KParts::ReadOnlyPart *part() const Q_DECL_OVERRIDE;
+    QUrl url() const Q_DECL_OVERRIDE;
+    bool openUrl(const OpenUrlRequest &request) Q_DECL_OVERRIDE;
+    void loadConfig(const KConfigGroup &, const QString &) Q_DECL_OVERRIDE;
+    void saveConfig(KConfigGroup &, const QString &) Q_DECL_OVERRIDE;
+
+    qreal zoomFactor() const Q_DECL_OVERRIDE;
+    bool zoomTextOnlyInFrame() const Q_DECL_OVERRIDE;
+
+public Q_SLOTS:
+    void slotHistoryForward() Q_DECL_OVERRIDE;
+    void slotHistoryBack() Q_DECL_OVERRIDE;
+    void slotReload() Q_DECL_OVERRIDE;
+    void slotStop() Q_DECL_OVERRIDE;
+
+    void slotZoomChangeInFrame(int frameId, qreal value);
+    void slotZoomTextOnlyInFrame(int frameId, bool textOnlyInFrame);
+    void slotCopyInFrame(int frameId);
+    void slotPrintInFrame(int frameId);
+    void slotPrintPreviewInFrame(int frameId);
+    void slotFindTextInFrame(int frameId);
+    void slotTextToSpeechInFrame(int frameId);
+    void slotSaveLinkAsInFrame(int frameId);
+    void slotCopyLinkAsInFrame(int frameId);
+    void slotSaveImageOnDiskInFrame(int frameId);
+    void slotCopyImageLocationInFrame(int frameId);
+    void slotBlockImageInFrame(int frameId);
+
+Q_SIGNALS:
+    void signalIconChanged(Akregator::Frame *, const QIcon &icon);
+
+private Q_SLOTS:
+    void slotTitleChanged(const QString &title);
+    void slotProgressChanged(int progress);
+    void slotLoadStarted();
+    void slotLoadFinished();
+private:
+    Akregator::ArticleViewerWidgetNg *mArticleViewerWidgetNg;
+
+};
+}
+
+#endif
