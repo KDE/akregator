3bb9072afb79576fa92eca623e3337e943c764c7
Montel Laurent <montel@kde.org>
2016-02-18 08:52:20 +0100

    Add webengine class

---
+++ src/articleviewer-ng/webengine/articleviewerwebenginewidgetng.cpp
@@ -0,0 +1,98 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "articleviewerwebenginewidgetng.h"
+
+#include "articleviewerng.h"
+#include <MessageViewer/FindBarWebView>
+
+#include <KActionCollection>
+#include <KLocalizedString>
+#include <QVBoxLayout>
+#include <QAction>
+
+#include <KPIMTextEdit/kpimtextedit/texttospeechwidget.h>
+
+#include <kpimtextedit/slidecontainer.h>
+
+using namespace Akregator;
+
+ArticleViewerWebEngineWidgetNg::ArticleViewerWebEngineWidgetNg(ArticleViewerNg *customViewer, KActionCollection *ac, QWidget *parent)
+    : QWidget(parent)
+{
+    mArticleViewerNg = customViewer;
+    initializeLayout(ac);
+}
+
+ArticleViewerWebEngineWidgetNg::ArticleViewerWebEngineWidgetNg(KActionCollection *ac, QWidget *parent)
+    : QWidget(parent),
+      mArticleViewerNg(Q_NULLPTR)
+{
+    initializeLayout(ac);
+}
+
+void ArticleViewerWebEngineWidgetNg::initializeLayout(KActionCollection *ac)
+{
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setMargin(0);
+    setLayout(layout);
+
+    mTextToSpeechWidget = new KPIMTextEdit::TextToSpeechWidget(this);
+    mTextToSpeechWidget->setObjectName(QStringLiteral("texttospeechwidget"));
+    layout->addWidget(mTextToSpeechWidget);
+
+    if (!mArticleViewerNg) {
+        mArticleViewerNg = new ArticleViewerNg(ac, this);
+    }
+    mArticleViewerNg->setObjectName(QStringLiteral("articleviewerng"));
+    layout->addWidget(mArticleViewerNg);
+
+    mSliderContainer = new KPIMTextEdit::SlideContainer(this);
+    mSliderContainer->setObjectName(QStringLiteral("slidercontainer"));
+    mFindBarWebView = new MessageViewer::FindBarWebView(mArticleViewerNg, this);
+    mFindBarWebView->setObjectName(QStringLiteral("findbarwebview"));
+    connect(mFindBarWebView, &MessageViewer::FindBarBase::hideFindBar, mSliderContainer, &KPIMTextEdit::SlideContainer::slideOut);
+    mSliderContainer->setContent(mFindBarWebView);
+    layout->addWidget(mSliderContainer);
+    connect(articleViewerNg(), &ArticleViewerNg::textToSpeech, this, &ArticleViewerWebEngineWidgetNg::slotSpeakText);
+    connect(articleViewerNg(), &ArticleViewerNg::findTextInHtml, this, &ArticleViewerWebEngineWidgetNg::slotFind);
+}
+
+ArticleViewerWebEngineWidgetNg::~ArticleViewerWebEngineWidgetNg()
+{
+
+}
+
+ArticleViewerNg *ArticleViewerWebEngineWidgetNg::articleViewerNg() const
+{
+    return mArticleViewerNg;
+}
+
+void ArticleViewerWebEngineWidgetNg::slotFind()
+{
+    if (mArticleViewerNg->hasSelection()) {
+        mFindBarWebView->setText(mArticleViewerNg->selectedText());
+    }
+    mSliderContainer->slideIn();
+    mFindBarWebView->focusAndSetCursor();
+}
+
+void ArticleViewerWebEngineWidgetNg::slotSpeakText()
+{
+    const QString text = mArticleViewerNg->selectedText();
+    mTextToSpeechWidget->say(text);
+}
---
+++ src/articleviewer-ng/webengine/articleviewerwebenginewidgetng.h
@@ -0,0 +1,60 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef ARTICLEVIEWERWEBENGINEWIDGETNG_H
+#define ARTICLEVIEWERWEBENGINEWIDGETNG_H
+
+#include <QWidget>
+#include "articleviewerng.h"
+#include "akregator_export.h"
+class KActionCollection;
+namespace MessageViewer
+{
+class FindBarWebView;
+}
+
+namespace KPIMTextEdit
+{
+class SlideContainer;
+class TextToSpeechWidget;
+}
+
+namespace Akregator
+{
+class AKREGATOR_EXPORT ArticleViewerWebEngineWidgetNg : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ArticleViewerWebEngineWidgetNg(ArticleViewerNg *customViewer, KActionCollection *ac, QWidget *parent);
+    explicit ArticleViewerWebEngineWidgetNg(KActionCollection *ac, QWidget *parent = Q_NULLPTR);
+    ~ArticleViewerWebEngineWidgetNg();
+
+    ArticleViewerNg *articleViewerNg() const;
+
+public Q_SLOTS:
+    void slotSpeakText();
+    void slotFind();
+private:
+    void initializeActions(KActionCollection *ac);
+    void initializeLayout(KActionCollection *ac);
+    MessageViewer::FindBarWebView *mFindBarWebView;
+    ArticleViewerNg *mArticleViewerNg;
+    KPIMTextEdit::TextToSpeechWidget *mTextToSpeechWidget;
+    KPIMTextEdit::SlideContainer *mSliderContainer;
+};
+}
+#endif // ARTICLEVIEWERWEBENGINEWIDGETNG_H
