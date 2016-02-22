e7f112f18472288be3d340c0c2eacf6cf6d7ebb4
Montel Laurent <montel@kde.org>
2016-02-22 16:43:26 +0100

    Test if file exists before to execute sed

--- kconf_update/akregator-15.08-kickoff.sh
+++ kconf_update/akregator-15.08-kickoff.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sed -i "s/\/akregator.desktop/\/org.kde.akregator.desktop/" `kf5-config --path config --locate kickoffrc`
+kickoffrcname=`kf5-config --path config --locate kickoffrc`
+if [ -f "$kickoffrcname" ]; then
+   sed -i "s/\/akregator.desktop/\/org.kde.akregator.desktop/" $kickoffrcname
+fi
