0f56efaa1746811a56135fc6e212846c4bf0baa2
Montel Laurent <montel@kde.org>
2016-05-04 08:18:14 +0200

    Move in each directory

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-../grantlee-extractor-pot-scripts/extract_strings_ki18n.py `find -name \*.html` >> src/html.cpp
+./grantlee-extractor-pot-scripts/extract_strings_ki18n.py `find -name \*.html` >> src/html.cpp
 $EXTRACTRC `find . -name "*.rc" -o -name "*.ui" -o -name "*.kcfg"` >> rc.cpp || exit 11
 $XGETTEXT $(find . -name "*.cpp" -o -name "*.h") -o $podir/akregator.pot
-rm -f rc.cpp src/html.cpp ../grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
+rm -f rc.cpp src/html.cpp ./grantlee-extractor-pot-scripts/grantlee_strings_extractor.pyc
