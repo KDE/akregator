4697446d950975b2d27cdfed25efb32c28503cdc
Montel Laurent <montel@kde.org>
2017-09-29 23:38:01 +0200

    Add parent to qdialogbuttonbox

--- src/feed/feedpropertiesdialog.cpp
+++ src/feed/feedpropertiesdialog.cpp
@@ -76,10 +76,10 @@ FeedPropertiesDialog::FeedPropertiesDialog(QWidget *parent, const QString &name)
 {
     setObjectName(name);
     setWindowTitle(i18n("Feed Properties"));
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
     widget = new FeedPropertiesWidget(this);
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
     QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
