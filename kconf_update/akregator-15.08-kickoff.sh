47a131bbdc07002f4f0fa7e64f271f561a040dda
Laurent Montel <montel@kde.org>
2021-10-15 06:55:13 +0200

    GIT_SILENT: remove old kconf_update

--- kconf_update/akregator-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/akregator.desktop/\/org.kde.akregator.desktop/" $kickoffrcname
-fi
