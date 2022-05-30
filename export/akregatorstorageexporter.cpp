KDE/akregator

commit d0eae1cf58506b63d65447322dc7da39b6afd5c0

Fix warning

export/akregatorstorageexporter.cpp
@@ -337,8 +337,9 @@ static void printUsage()
 }
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+    QCoreApplication app(argc, argv);
     const QString backend = QStringLiteral("metakit");
 
     if (argc < 2) {
@@ -381,5 +382,5 @@ int main(int argc, char **argv)
 
     serialize(storage, url, &out);
 
-    return 0;
+    return app.exec();
 }

commit 83463588de2cb828ba2fdbeaa705c664617f1a36

Don't enter event loop in akregatorstorageexporter
There are no events that could exit the loop, so the application
just hangs indefinitely.

Fixes: d0eae1cf58506b63d65447322dc7da39b6afd5c0 ("Fix warning")
Signed-off-by: Alexander Miller <alex.miller@gmx.de>

export/akregatorstorageexporter.cpp
@@ -322,5 +322,5 @@ int main(int argc, char *argv[])
 
     serialize(&storage, url, &out);
 
-    return app.exec();
+    return 0;
 }