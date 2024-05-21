3ee04cb6fd4cd0bd334410af7e6ed3f8cd3d7e7d
Laurent Montel <montel@kde.org>
2024-05-21 07:56:38 +0200

    Use KIconThemes and KStyleManager if available during build-time
    This can benefit platforms like Windows so breeze's icon can be used
    if the platform don't offer icon theme support out-of-the-box.
    
    See also: https://cullmann.io/posts/kde-applications-and-icons/

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,16 @@
 
 #include <QDBusInterface>
 
+#define HAVE_KICONTHEME __has_include(<KIconTheme>)
+#if HAVE_KICONTHEME
+#include <KIconTheme>
+#endif
+
+#define HAVE_STYLE_MANAGER __has_include(<KStyleManager>)
+#if HAVE_STYLE_MANAGER
+#include <KStyleManager>
+#endif
+
 namespace Akregator
 {
 class Application : public KontactInterface::PimUniqueApplication
@@ -67,11 +77,21 @@ int Application::activate(const QStringList &args, const QString &workingDir)
 
 int main(int argc, char **argv)
 {
+#if HAVE_KICONTHEME && (KICONTHEMES_VERSION >= QT_VERSION_CHECK(6, 3, 0))
+    KIconTheme::initTheme();
+#endif
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     Akregator::Application app(argc, &argv);
     KLocalizedString::setApplicationDomain(QByteArrayLiteral("akregator"));
 
     KCrash::initialize();
+#if HAVE_STYLE_MANAGER
+    KStyleManager::initStyle();
+#else // !HAVE_STYLE_MANAGER
+#if defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+    QApplication::setStyle(QStringLiteral("breeze"));
+#endif // defined(Q_OS_MACOS) || defined(Q_OS_WIN)
+#endif // HAVE_STYLE_MANAGER
 
     app.setDesktopFileName(QStringLiteral("org.kde.akregator"));
     Akregator::AboutData about;
