631800316557a0e0f65efff23eaa4db6ff3009ea
Laurent Montel <montel@kde.org>
2024-08-06 19:28:43 +0200

    USe PimCommonActivities

--- src/activities/activitiesmanager.cpp
+++ src/activities/activitiesmanager.cpp
@@ -5,26 +5,11 @@
 */
 
 #include "activitiesmanager.h"
-#include "akregator_plasma_activities_debug.h"
 #include "akregatorconfig.h"
-#include <PlasmaActivities/Consumer>
 using namespace Akregator;
 ActivitiesManager::ActivitiesManager(QObject *parent)
-    : QObject{parent}
-    , mActivitiesConsumer(new KActivities::Consumer(this))
+    : PimCommonActivities::ActivitiesBaseManager{parent}
 {
-    connect(mActivitiesConsumer, &KActivities::Consumer::currentActivityChanged, this, [this](const QString &activityId) {
-        qCDebug(AKREGATOR_PLASMA_ACTIVITIES_LOG) << " switch to activity " << activityId;
-        Q_EMIT activitiesChanged();
-    });
-    connect(mActivitiesConsumer, &KActivities::Consumer::activityRemoved, this, [this](const QString &activityId) {
-        qCDebug(AKREGATOR_PLASMA_ACTIVITIES_LOG) << " Activity removed " << activityId;
-        Q_EMIT activitiesChanged();
-    });
-    connect(mActivitiesConsumer, &KActivities::Consumer::serviceStatusChanged, this, &ActivitiesManager::activitiesChanged);
-    if (mActivitiesConsumer->serviceStatus() != KActivities::Consumer::ServiceStatus::Running) {
-        qCWarning(AKREGATOR_PLASMA_ACTIVITIES_LOG) << "Plasma activities is not running: " << mActivitiesConsumer->serviceStatus();
-    }
 }
 
 ActivitiesManager::~ActivitiesManager() = default;
@@ -32,32 +17,6 @@ ActivitiesManager::~ActivitiesManager() = default;
 bool ActivitiesManager::enabled() const
 {
     return Akregator::Settings::self()->plasmaActivitySupport();
-}
-
-bool ActivitiesManager::isInCurrentActivity(const QStringList &lst) const
-{
-    if (mActivitiesConsumer->serviceStatus() == KActivities::Consumer::ServiceStatus::Running) {
-        if (lst.contains(mActivitiesConsumer->currentActivity())) {
-            return true;
-        } else {
-            const QStringList activities = mActivitiesConsumer->activities();
-            auto index = std::find_if(activities.constBegin(), activities.constEnd(), [lst](const QString &str) {
-                return lst.contains(str);
-            });
-            // Account doesn't contains valid activities => show it.
-            if (index == activities.constEnd()) {
-                return true;
-            }
-            return false;
-        }
-    } else {
-        return true;
-    }
-}
-
-QString ActivitiesManager::currentActivity() const
-{
-    return mActivitiesConsumer->currentActivity();
 }
 
 #include "moc_activitiesmanager.cpp"
--- src/activities/activitiesmanager.h
+++ src/activities/activitiesmanager.h
@@ -7,51 +7,16 @@
 #pragma once
 
 #include "akregator_export.h"
+#include <PimCommonActivities/ActivitiesBaseManager>
 #include <QObject>
-namespace KActivities
-{
-class Consumer;
-}
 namespace Akregator
 {
-class AKREGATOR_EXPORT ActivitiesManager : public QObject
+class AKREGATOR_EXPORT ActivitiesManager : public PimCommonActivities::ActivitiesBaseManager
 {
     Q_OBJECT
 public:
-    struct ActivitySettings {
-        QStringList activities;
-        bool enabled = false;
-        [[nodiscard]] bool contains(const QString &str) const
-        {
-            return activities.contains(str);
-        }
-
-        void changeActivities(bool added, const QString &currentActivity)
-        {
-            if (added) {
-                if (!activities.contains(currentActivity)) {
-                    activities.append(currentActivity);
-                }
-            } else {
-                if (activities.contains(currentActivity)) {
-                    activities.removeAll(currentActivity);
-                }
-            }
-            enabled = true;
-        }
-    };
-
     explicit ActivitiesManager(QObject *parent = nullptr);
     ~ActivitiesManager() override;
-    [[nodiscard]] bool enabled() const;
-
-    [[nodiscard]] bool isInCurrentActivity(const QStringList &lst) const;
-
-    [[nodiscard]] QString currentActivity() const;
-Q_SIGNALS:
-    void activitiesChanged();
-
-private:
-    KActivities::Consumer *const mActivitiesConsumer;
+    [[nodiscard]] bool enabled() const override;
 };
 }
