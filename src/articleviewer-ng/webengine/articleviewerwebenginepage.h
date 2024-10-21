2eb871ce1dc3d2a53f3fcf520b6ecca6af3d00f3
Laurent Montel <montel@kde.org>
2024-10-21 08:25:47 +0200

    GIT_SILENT: it compiles fine without deprecated methods

--- src/articleviewer-ng/webengine/articleviewerwebenginepage.h
+++ src/articleviewer-ng/webengine/articleviewerwebenginepage.h
@@ -20,7 +20,11 @@ protected:
     bool acceptNavigationRequest(const QUrl &url, NavigationType type, bool isMainFrame) override;
 
 private:
+#if QT_VERSION >= QT_VERSION_CHECK(6, 8, 0)
+    void slotFeaturePermissionRequested(QWebEnginePermission feature);
+#else
     void slotFeaturePermissionRequested(const QUrl &url, QWebEnginePage::Feature feature);
+#endif
     void onLoadProgress();
 };
 }
