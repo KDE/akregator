1319b1869225d811879f178b47fab416cc2702f7
Laurent Montel <montel@kde.org>
2024-11-16 13:40:55 +0100

    Add 6.4.0 info

--- src/whatsnew/whatsnewtranslations.cpp
+++ src/whatsnew/whatsnewtranslations.cpp
@@ -13,7 +13,7 @@ WhatsNewTranslations::~WhatsNewTranslations() = default;
 // Use by newFeaturesMD5
 QList<KLazyLocalizedString> WhatsNewTranslations::lastNewFeatures() const
 {
-    const QList<KLazyLocalizedString> info{kli18n("Implement Plasma Activity."), kli18n("Add new whatsnew widget.")};
+    const QList<KLazyLocalizedString> info{kli18n("Add windows support")};
     return info;
 }
 
@@ -22,12 +22,19 @@ QList<PimCommon::WhatsNewInfo> WhatsNewTranslations::createWhatsNewInfo() const
     QList<PimCommon::WhatsNewInfo> listInfo;
     {
         PimCommon::WhatsNewInfo info;
+        const QStringList lst{i18n("Implement Plasma Activity."), i18n("Add new whatsnew widget.")};
+        info.setNewFeatures(lst);
+        info.setVersion(QStringLiteral("6.3.0"));
+        listInfo.append(std::move(info));
+    }
+    {
+        PimCommon::WhatsNewInfo info;
         QStringList lst;
         for (const KLazyLocalizedString &l : lastNewFeatures()) {
             lst += l.toString();
         }
         info.setNewFeatures(lst);
-        info.setVersion(QStringLiteral("6.3.0"));
+        info.setVersion(QStringLiteral("6.4.0"));
         listInfo.append(std::move(info));
     }
     return listInfo;
